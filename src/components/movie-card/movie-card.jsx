201b1626992a0274b89baf7073a24479e5aa652b
megidolaonn <101783502+megidolaonn@users.noreply.github.com>
2022-09-02 04:22:55 -0400

    added favorites button

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -2,10 +2,28 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Card, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 export class MovieCard extends React.Component {
+
+  addFavorite(movie) {
+    const user = localStorage.getItem('user');
+    const token = localStorage.getItem('token');
+    axios.post(
+      `https://myflix17507.herokuapp.com/users/${user}/movies/${movie._id}`, {}, {
+      headers: { Authorization: `Bearer ${token}` }
+    })
+      .then((response) => {
+        console.log(response);
+        alert("Movie added to favorites.");
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
   render() {
-    const { movie, onMovieClick } = this.props;
+    const { movie } = this.props;
 
     return (
       <Card>
@@ -16,6 +34,7 @@ export class MovieCard extends React.Component {
           <Link to={`/movies/${movie._id}`}>
             <Button variant="link">Open</Button>
           </Link>
+          <Button onClick={() => this.addFavorite(movie)}>Add to Favorites</Button>
         </Card.Body>
       </Card>
     );
