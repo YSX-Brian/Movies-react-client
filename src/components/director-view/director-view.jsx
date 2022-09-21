e03405b06678c0b5a2d2193215708a8941a4ae0b
megidolaonn <101783502+megidolaonn@users.noreply.github.com>
2022-09-21 19:47:53 -0400

    changed view layout

--- src/components/director-view/director-view.jsx
+++ src/components/director-view/director-view.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Form, Button, Card, CardGroup, Container, Row, Col } from 'react-bootstrap';
+import { Button, Card, CardGroup, Container, Row, Col } from 'react-bootstrap';
 
 export class DirectorView extends React.Component {
 
@@ -14,8 +14,8 @@ export class DirectorView extends React.Component {
               <Card>
                 <Card.Body className="text-center">
                   <Card.Title>{director.Name}</Card.Title>
+                  <Card.Text>Born: {director.Birth}</Card.Text>
                   <Card.Text>{director.Bio}</Card.Text>
-                  <Card.Text>{director.Birth}</Card.Text>
                   <Button onClick={() => { onBackClick(); }} variant="dark">Back</Button>
                 </Card.Body>
               </Card>
